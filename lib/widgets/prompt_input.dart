a004eec2b6e1fc8881d801f540a2896aa8d1bafd
Dongjin <dongqui9@gmail.com>
2024-11-25 19:10:36 +0900

    feat: prompt UI

--- lib/widgets/prompt_input.dart
+++ lib/widgets/prompt_input.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 
 class PromptInput extends StatelessWidget {
-  final Function(String) onSubmit;
-
+  final Function() onSubmit;
+  final Function(String) onChange;
   const PromptInput({
     super.key,
     required this.onSubmit,
+    required this.onChange,
   });
 
   @override
@@ -19,30 +20,31 @@ class PromptInput extends StatelessWidget {
               color: const Color(0xFF141414),
               borderRadius: BorderRadius.circular(12),
             ),
-            child: const TextField(
-              maxLines: 3,
-              decoration: InputDecoration(
+            child: TextField(
+              maxLines: 4,
+              onChanged: onChange,
+              decoration: const InputDecoration(
                 border: InputBorder.none,
                 contentPadding: EdgeInsets.fromLTRB(16, 16, 56, 16),
-                labelText: 'Enter prompt',
+                labelText: 'Prompt',
                 labelStyle: TextStyle(
                   color: Colors.white,
                   fontSize: 13,
                   letterSpacing: 1.0,
                   fontWeight: FontWeight.w700,
                 ),
-                hintText: 'Write your feedback...',
+                hintText: 'Describe the image you want',
                 hintStyle: TextStyle(
                   color: Colors.white24,
                   fontSize: 16,
                 ),
                 floatingLabelBehavior: FloatingLabelBehavior.always,
               ),
-              style: TextStyle(
+              style: const TextStyle(
                 color: Colors.white,
                 fontSize: 16,
               ),
-              cursorColor: Color(0xFF4D4EE8),
+              cursorColor: const Color(0xFF4D4EE8),
             ),
           ),
           Positioned(
@@ -50,7 +52,7 @@ class PromptInput extends StatelessWidget {
             bottom: 8,
             child: IconButton(
               onPressed: () {
-                // TODO: onSubmit 호출
+                onSubmit();
               },
               icon: const Icon(
                 Icons.send_rounded,
